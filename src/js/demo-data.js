9fc9ead1724448a47c71cd80c058c00d2a0128ca
RGB-0000FF <uranuss.lb@gmail.com>
2026-05-25 21:04:42 +0300

    Sync demo logs to Sydney standard

--- src/js/demo-data.js
+++ src/js/demo-data.js
@@ -271,7 +271,7 @@ const HLS_PLAYBACK_CONFIG = {
   maxMaxBufferLength: 90,
   backBufferLength: 30
 };
-const DATA_ASSET_VERSION = '20260525-chunk-reframe';
+const DATA_ASSET_VERSION = '20260525-all-demo-standard';
 let runEventsManifestPath = '';
 const DEFAULT_DEMO_ID = 'sydney-opera-house';
 let hlsPreloadThrottleBound = false;
