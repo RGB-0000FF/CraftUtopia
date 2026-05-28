9408ce770c27c851f75adbacb36af655ecc03c79
RGB-0000FF <uranuss.lb@gmail.com>
2026-05-28 12:32:41 +0300

    Fix demo timestamp ordering

--- src/js/demo-data.js
+++ src/js/demo-data.js
@@ -271,7 +271,7 @@ const HLS_PLAYBACK_CONFIG = {
   maxMaxBufferLength: 90,
   backBufferLength: 30
 };
-const DATA_ASSET_VERSION = '20260528-build-region-weighted-milestone';
+const DATA_ASSET_VERSION = '20260528-demo-timestamp-audit';
 let runEventsManifestPath = '';
 const DEFAULT_DEMO_ID = 'sydney-opera-house';
 let hlsPreloadThrottleBound = false;
